5d5179b50bec52de98c9db2de971f785888da4bd
pratu880 <abhishekjagtap149@gmail.com>
2025-08-29 13:36:52 +0530

    added task 1 n task2

---
+++ task1.py
@@ -0,0 +1,8 @@
+# check if a number is even or odd using if else
+num = int(input('Enter a number: '))
+if num%2==0:
+    print(num,"is even number.")
+elif num%2==1:
+    print(num,"is odd number.")
+else:
+    print("Enter a valid output.")
